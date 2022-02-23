dfe025e1038ff68813035fbe62ae7a4d83fa4b22
Ines Ortega <iortega@gradiant.org>
2022-02-23 13:40:43 +0100

    add utils.py

---
+++ src/utils/utils.py
@@ -0,0 +1,148 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import scipy
+from src.NeuralGAM.ngam import NeuralGAM, load_model
+from scipy.stats import truncnorm
+
+#todo delete in prod!!
+np.random.seed = 42
+      
+""" PLOTTING aux functions """
+def plot_predicted_vs_real(dataframe_list: list, legends: list, title:str, output_path=None):
+    fig, axs = plt.subplots(1, len(dataframe_list))
+    fig.suptitle(title, fontsize=16)
+    for i, term in enumerate(dataframe_list):
+        axs[i].plot(dataframe_list[i])
+        axs[i].grid()
+        axs[i].set_title(legends[i])
+
+    if output_path:
+        plt.savefig(output_path, dpi = 100)
+        fig = plt.gcf()
+        plt.show(block=False)
+    
+def plot_partial_dependencies(x, fs, title:str, output_path=None):
+    fig, axs = plt.subplots(nrows=1, ncols=len(fs.columns))
+    fig.suptitle(title, fontsize=16)
+    for i, term in enumerate(fs.columns):
+        
+        data = pd.DataFrame()
+        
+        data['x'] = x[x.columns[i]]
+        data['y']= fs[fs.columns[i]]
+        # calculate confidence interval at 95%
+        ci = 1.96 * np.std(data['y'])/np.sqrt(len(data['x']))
+        
+        data['y+ci'] = data['y'] + ci
+        data['y-ci'] = data['y'] - ci
+        sns.lineplot(data = data, x='x', y='y', color='b', ax=axs[i])
+        sns.lineplot(data = data, x='x', y='y-ci', color='r', linestyle='--', ax=axs[i])
+        sns.lineplot(data = data, x='x', y='y+ci', color='r', linestyle='--', ax=axs[i])
+        axs[i].grid()
+        axs[i].set_title("f[{0}]".format(i))
+  
+    if output_path:
+        plt.savefig(output_path, dpi = 100)
+        fig = plt.gcf()
+        plt.show(block=False)
+
+
+def split(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=False)
+    return X_train.reset_index(drop=True), X_test.reset_index(drop=True), y_train.reset_index(drop=True).squeeze(), y_test.reset_index(drop=True).squeeze()
+
+
+def save(X_train,X_test,y_train,y_test, output_folder):
+    import os
+    output_path = os.path.normpath(os.path.abspath(output_folder))
+    
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    
+    X_train.to_csv(os.path.join(output_path, "x_train.csv"), index=False)
+    X_test.to_csv(os.path.join(output_path, "x_test.csv"), index=False)
+    y_train.to_csv(os.path.join(output_path, "y_train.csv"), index=False)
+    y_test.to_csv(os.path.join(output_path, "y_test.csv"), index=False)
+    
+        
+""" DATA GENERATION """
+
+def generate_err(nrows:int, data_type:str, X:pd.DataFrame):
+    if data_type == "homoscedastic":
+        err = np.random.normal(loc=0, scale=0.2, size=nrows)
+    elif data_type == "heteroscedastic":
+        x_mean = X.mean(axis=1)
+        err = np.random.normal(loc=0, scale=np.abs(0.1*x_mean), size=nrows)
+
+    print("\n Intercept: {0} data".format(data_type))
+    print(pd.DataFrame(err).describe())
+    return err
+
+
+def generate_normal_data(nrows, data_type, output_path=""):
+    
+    x1 = truncnorm(a=-10, b=10, loc=0.0, scale=1.0).rvs(nrows)
+    x2 = truncnorm(a=-10, b=10, loc=0.0, scale=1.0).rvs(nrows)
+    x3 = truncnorm(a=-10, b=10, loc=0.0, scale=1.0).rvs(nrows)
+    b = np.ones(nrows)* 2
+
+    X = pd.DataFrame([x1,x2,x3,b]).transpose()
+    X_func = pd.DataFrame([x1*x1, 2*x2, np.sin(x3), b]).transpose()
+    plot_partial_dependencies(X, X_func, "Original f(x)", output_path=output_path + "/original_f.png")
+    
+    print("y = f(x1) + f(x2) + f(x3) + b =  x1^2 + 2x2 + sin(x3) + b")
+    
+    y = pd.Series(x1*x1 + 2*x2 + np.sin(x3) + b)
+    err = generate_err(nrows=nrows, data_type=data_type, X=X)
+    y = y + err
+    return X, y
+
+def generate_uniform_data(nrows, data_type, output_path = ""):
+    
+    x1 = np.array(np.random.uniform(low=-10, high=10, size=nrows))
+    x2 = np.array(np.random.uniform(low=-10, high=10, size=nrows))
+    x3 = np.array(np.random.uniform(low=-10, high=10, size=nrows))
+    b = np.ones(nrows)* 2
+
+    X = pd.DataFrame([x1,x2,x3,b]).transpose()
+    X_func = pd.DataFrame([x1*x1, 2*x2, np.sin(x3), b]).transpose()
+    plot_partial_dependencies(X, X_func, "Original f(x)", output_path=output_path + "/original_f.png")
+    print("y = f(x1) + f(x2) + f(x3) + b =  x1^2 + 2x2 + sin(x3) + b")
+
+    y = pd.Series(x1*x1 + 2*x2 + np.sin(x3) + b)
+    err = generate_err(nrows=nrows, data_type=data_type, X=X)
+
+    y = y + err
+
+    return X, y
+
+
+def generate_data(data_type, distribution, nrows=25000, output_folder = ""):
+    """
+        Returns a pair of X,y to be used with NeuralGAM
+        :param: data_type: homogeneity of variance on the intercept term {homoscedastic, heteroscedastic}
+        :param: distribution: generate normal or uniform distributed X data {uniform, normal}
+        :param: nrows: data size (number of rows)
+        :param: output_folder: folder path to save the generated files locally in CSV format
+        :return: X: pandas Dataframe object with generated X (one column per feature). Xs follow a normal distribution
+        :return: y: pandas Series object with computed y, with a normal distribution + homoskedastic residual
+    """
+    
+    if distribution == "uniform":
+        X, y = generate_uniform_data(nrows, data_type, output_path=output_folder)
+
+    elif distribution == "normal":
+        X, y = generate_normal_data(nrows, data_type, output_path=output_folder)   
+
+    return X, y
+    
+def compute_edf(a: pd.Series, b: pd.Series):
+    return scipy.stats.ttest_rel(a, b)
+        
