fixing partial dependencies computation

main.py
@@ -9,7 +9,7 @@ from src.utils.utils import generate_data, plot_confusion_matrix, plot_multiple_
 from src.NeuralGAM.ngam import NeuralGAM, load_model, apply_link
 import mlflow
 from sklearn.metrics import mean_squared_error
-       
+import pandas as pd
 parser = argparse.ArgumentParser()
 
 subparsers = parser.add_subparsers(help='Choose wether to use Linear (linear) or Logistic (logistic) Regression')
@@ -89,8 +89,8 @@ if __name__ == "__main__":
     
     #setup_mlflow()
     log_mlflow = False
-    
-    path = os.path.normpath(os.path.abspath("./results_test/{0}".format("_".join(list(variables.values())))))
+    rel_path = "./results_test/{0}".format("_".join(list(variables.values())))
+    path = os.path.normpath(os.path.abspath(rel_path))
     if not os.path.exists(path):
         os.mkdir(path)
         
@@ -114,32 +114,30 @@ if __name__ == "__main__":
             
             if link == "logistic":
                 y_train_binomial = np.random.binomial(n=1, p=y_train, size=y_train.shape[0])
-                ngam.fit(X_train = X_train, y_train = y_train_binomial, max_iter = 5, convergence_threshold=0.1)
-                print("MSE y_train / ngam.y = {0}".format(mean_squared_error(y_train, ngam.y)))
+                ngam.fit(X_train = X_train, y_train = y_train_binomial, max_iter = 5, convergence_threshold=0.05)
+                print("MSE y_train / ngam.y = {0}".format(mean_squared_error(y_train, ngam.y)))                
             else:         
                 ngam.fit(X_train = X_train, y_train = y_train, max_iter = 5, convergence_threshold=0.05)
                 print("MSE y_train / ngam.y = {0}".format(mean_squared_error(y_train, ngam.y)))
 
-            training_mse = ngam.training_mse
-            model_path = ngam.save_model(path)
-
+            #model_path = ngam.save_model(path)
+            [mlflow.log_metric("training_mse", float(i)) for i in ngam.training_mse]
+            #mlflow.pyfunc.log_model(python_model=ngam, artifact_path='neuralGAM')
+            
         y_pred = ngam.predict(X_test)
         
         #Calculo el error entre y_pred (prob teorica) y el resultado    
         mse = mean_squared_error(y_test, y_pred)
         variables["MSE"] = mse
+        mlflow.log_params(dict(variables))
                
-        training_fs = ngam.get_partial_dependencies(X_train)
-        test_fs = ngam.get_partial_dependencies(X_test)
-        
         print("Finished predictions...Plotting results... ")
         print(variables)
+        
         if link == "logistic":
             
-            # put theorethical fs on the scale of the distribution mean by applying the inverse link function column-wise
-            #fs = fs.apply(lambda x: apply_link(link, x), axis=0)
-            #fs = fs - fs.mean()
-            
+            training_fs = ngam.get_partial_dependencies(X_train, xform=False)
+            test_fs = ngam.get_partial_dependencies(X_test, xform=False)
             x_list = [X_train, X_test]
             fs_list = [training_fs, test_fs]
             legends = ["X_train", "X_test"]
@@ -170,22 +168,19 @@ if __name__ == "__main__":
             metrics["tnr"] = tn / (tn + fp)
             metrics["accuracy"] = (metrics["tp"] + metrics["tn"]) / (metrics["tp"] + metrics["tn"] + metrics["fp"] + metrics["fn"])
             metrics["f1"] = 2 * ((metrics["precision"]  * metrics["recall"]) / (metrics["precision"]  + metrics["recall"]))
-
-            import pandas as pd
-            pd.DataFrame([metrics]).to_csv(path + '/classification-report.csv', index=False)
+            mlflow.log_metrics(metrics)
+            pd.DataFrame([metrics]).to_csv(path + '/classification-report.csv', index=False)      
+        
+        # Put all fs on the scale of the distribution mean by applying the inverse link function column-wise
+        fs = fs.apply(lambda x: apply_link(link, x))
+        training_fs = ngam.get_partial_dependencies(X_train, xform=True)
+        test_fs = ngam.get_partial_dependencies(X_test, xform=True)
         
         x_list = [X, X_train, X_test]
         fs_list = [fs, training_fs, test_fs]
         legends = ["X", "X_train", "X_test"]
+        
         plot_multiple_partial_dependencies(x_list=x_list, f_list=fs_list, legends=legends, title=variables, output_path=path + "/functions.png")
-    
-    if log_mlflow:
-        mlflow.log_artifact(model_path)
-        mlflow.log_metrics(metrics)
-        mlflow.log_params(dict(variables))
-        mlflow.log_metrics({"training_mses": [float(i) for i in ngam.training_mse]})
-        mlflow.log_artifact(path + "/y_histogram.png")
-        mlflow.log_artifact(path + "/functions.png")
-        mlflow.log_artifact(path + "/functions_logistic.png")
-    
-    plt.show(block=True)
+        mlflow.log_artifacts(path)
+            
+    #plt.show(block=True)

src/NeuralGAM/ngam.py
@@ -14,6 +14,7 @@ TfInput = Union[np.ndarray, tf.Tensor]
 import dill
 import os
 from sklearn.preprocessing import MinMaxScaler
+from mlflow.pyfunc import PythonModel 
 
 class NeuralGAM(tf.keras.Model):
     """
@@ -79,7 +80,7 @@ class NeuralGAM(tf.keras.Model):
         self.muhat = y_train.mean()
         self.eta = inv_link(self.link, self.muhat)
         Z = inv_link(self.link, y_train) - self.eta
-           
+        
         # Start backfitting algorithm
         while not converged and it < max_iter:
             #for each feature
@@ -115,10 +116,12 @@ class NeuralGAM(tf.keras.Model):
             it+=1
         
         # Reconstruct learnt y
-        self.y = apply_link(self.link, g.sum(axis=1) + self.eta)
+        self.eta = apply_link(self.link, self.eta)
+        self.y = apply_link(self.link, g.sum(axis=1)) + self.eta
+            
         return
 
-    def get_partial_dependencies(self, X: pd.DataFrame):
+    def get_partial_dependencies(self, X: pd.DataFrame, xform=True):
         """ 
         Get the partial dependencies for each feature in X
         xform : bool, default: True, whether to apply the inverse link function and return values
@@ -127,11 +130,13 @@ class NeuralGAM(tf.keras.Model):
         output = pd.DataFrame(columns=range(len(X.columns)))
         for i in range(len(X.columns)):
             output[i] = pd.Series(self.feature_networks[i].predict(X[X.columns[i]]).flatten())
+        if xform:
+            output = output.apply(lambda x: apply_link(self.link, x))
         return output
             
     def predict(self, X: pd.DataFrame):
         """Computes Neural GAM output by computing a linear combination of the outputs of individual feature networks."""
-        output = self.get_partial_dependencies(X)
+        output = self.get_partial_dependencies(X, xform=False)
         y = apply_link(self.link, output.sum(axis=1) + self.eta)
         return y
         
@@ -139,14 +144,42 @@ class NeuralGAM(tf.keras.Model):
     def save_model(self, output_path):
         if not os.path.exists(output_path):
             os.mkdir(output_path)
-        with open(output_path + "/model.ngam", "wb") as file:
-            dill.dump(self, file, dill.HIGHEST_PROTOCOL)
-            return output_path + "/model.ngam"
+        if os.path.exists(output_path + "/model.ngam"):
+            os.remove(output_path + "/model.ngam")
+        #with open(output_path + "/model.ngam", "wb") as file:
+            #dill.dump(self, file, dill.HIGHEST_PROTOCOL)
+        import mlflow
+        mlflow.pyfunc.save_model(python_model=self, path=output_path + "/model.ngam")
+        return output_path + "/model.ngam"
       
-          
+    
+def compute_loss(type, actual, pred):
+    # calculate binary cross entropy
+    if type == "binary_cross_entropy":
+        sum_score = 0.0
+        for i in range(len(actual)):
+            sum_score += actual[i] * np.log(1e-15 + pred[i])
+        mean_sum_score = 1.0 / len(actual) * sum_score
+        return -mean_sum_score
+    elif type == "mse":
+        return mean_squared_error(actual, pred)    
+    elif type == "rmse":
+        return mean_squared_error(actual, pred, squared=True)
+    elif type == "categorical_cross_entropy":
+        sum_score = 0.0
+        for i in range(len(actual)):
+            for j in range(len(actual[i])):
+                sum_score += actual[i][j] * np.log(1e-15 + pred[i][j])
+        mean_sum_score = 1.0 / len(actual) * sum_score
+        return -mean_sum_score
+    else:
+        raise ValueError("Invalid loss type")
+    
 def load_model(model_path) -> NeuralGAM:
-    with open(model_path, "rb") as file:
-        return dill.load(file)
+    """with open(model_path, "rb") as file:
+        return dill.load(file)"""
+    import mlflow
+    return mlflow.pyfunc.load_model(model_path)
 
 
 def apply_link(link, a):
@@ -164,6 +197,5 @@ def inv_link(link, a):
             a[a == 0] += .01 # edge case for log link, inverse link, and logit link
             a[a == 1] -= .01 # edge case for logit link
         return np.log(a) - np.log(1 - a)
-        #return np.log(a / (1-a))
     else:   # identity / linear
         return a

src/utils/utils.py
@@ -165,11 +165,11 @@ def generate_normal_data(nrows, data_type, link, output_path=""):
     x1 = get_truncated_normal(mean=0.0, sd=1.0, low=-5, upp=5, nrows=nrows)
     x2 = get_truncated_normal(mean=0.0, sd=1.0, low=-5, upp=5, nrows=nrows)
     x3 = get_truncated_normal(mean=0.0, sd=1.0, low=-5, upp=5, nrows=nrows)
-    beta0 = np.ones(nrows)
+    beta0 = np.ones(nrows) * 2
     
     X = pd.DataFrame([x1,x2,x3]).transpose()
     fs = pd.DataFrame([x1*x1, 2*x2, np.sin(x3)]).transpose()
-    print("y = beta0 + f(x1) + f(x2) + f(x3) =  beta0 + x1^2 + 2x2 + sin(x3)")
+    print("y = beta0 + f(x1) + f(x2) + f(x3) =  2 + x1^2 + 2x2 + sin(x3)")
     
     # mean-center each f 
     fs = fs - fs.mean()
@@ -184,7 +184,7 @@ def generate_uniform_data(nrows, data_type, link, output_path = ""):
     x1 = np.array(np.random.uniform(low=-5, high=5, size=nrows))
     x2 = np.array(np.random.uniform(low=-5, high=5, size=nrows))
     x3 = np.array(np.random.uniform(low=-5, high=5, size=nrows))
-    beta0 = np.ones(nrows)
+    beta0 = np.ones(nrows) * 2
     
     X = pd.DataFrame([x1,x2,x3]).transpose()
     fs = pd.DataFrame([x1*x1, 2*x2, np.sin(x3)]).transpose()