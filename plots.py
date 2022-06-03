7638836d6b7e577dbafcef9940906cc4aa813d11
iortega <iortega@gradiant.org>
2022-06-03 14:00:27 +0200

    improved plotting

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-matplotlib.use('TkAgg') 
+matplotlib.use('Agg') 
 import os
 import seaborn as sns
 
@@ -23,16 +23,12 @@ if __name__ == "__main__":
             fs_estimated_i = pd.read_csv("./results/{0}/{1}/fs_test_estimated.csv".format(j, type), index_col=0).reset_index(drop=True)
             
             for i, f in enumerate(f_list):
-                #pd.concat(f_list[i][j], fs_estimated_i[str(i)], axis=1)
-                #f_list[i][j] = fs_estimated_i[str(i)]
                 f_list[i] = pd.concat([f_list[i], fs_estimated_i[str(i)]], axis=1)
-
-            
-            q975 = q975.append(fs_estimated_i.quantile(0.975).transpose())
-            q025 = q025.append(fs_estimated_i.quantile(0.025).transpose())
 
         for i, f in enumerate(f_list):
             mean_estimations[i] = f_list[i].mean(axis=1)
+            q975[i] = f_list[i].quantile(0.975, axis=1)
+            q025[i] = f_list[i].quantile(0.025, axis=1)
         
         return mean_estimations.reset_index(drop=True), q975.reset_index(drop=True), q025.reset_index(drop=True)
 
@@ -49,7 +45,7 @@ if __name__ == "__main__":
 
     fig, axs = plt.subplots(nrows=1, ncols=1)
     
-    plot_types = ["homoscedastic_uniform_gaussian", "heteroscedastic_uniform_gaussian","uniform_binomial"]
+    plot_types = ["homoscedastic_uniform_gaussian", "heteroscedastic_uniform_gaussian", "uniform_binomial"]
 
     for type in plot_types:
         try:
@@ -64,7 +60,7 @@ if __name__ == "__main__":
             fig, axs = plt.subplots(nrows=1, ncols=3)
 
             print("Generating " + type + " plots")
-            fig.suptitle("Theoretical training data and learned functions (100 iterations) \n " + type, fontsize=10)
+            fig.suptitle("Theoretical training data and estimated functions (1000 iterations) \n " + type, fontsize=10)
             
             mean_estimations, q975, q025 = compute_mean_estimations(X_train)
 
@@ -84,35 +80,35 @@ if __name__ == "__main__":
                             color='green', linestyle='--', alpha=0.7)
                 
                 # calculate confidence interval at 95%
+                """
                 ci = 1.96 * np.std(data['y'])/np.sqrt(len(data['x']))
+                
                 data['y+ci'] = data['y'] + ci
                 data['y-ci'] = data['y'] - ci
                 sns.lineplot(data = data, x='x', y='y+ci', color='grey', linestyle='--', alpha=0.5, ax=axs[i])
                 sns.lineplot(data = data, x='x', y='y-ci', color='grey', linestyle='--', alpha=0.5, ax=axs[i])
-
+                """
                 # draw quantiles
-                """q = pd.DataFrame()
-                q['x'] =  np.random.uniform(-3,3,q975.shape[0])
-                q['q975'] = q975[str(i)]
-                q['q025'] = q025[str(i)]
-                sns.lineplot(data = q, x='x', y='q975', color='grey', linestyle='--', alpha=0.5, ax=axs[i])
-                sns.lineplot(data = q, x='x', y='q025', color='grey', linestyle='--', alpha=0.5, ax=axs[i])"""
+                data['q975'] = q975[i]
+                data['q025'] = q025[i]
+                sns.lineplot(data = data, x='x', y='q975', color='grey', linestyle='--', alpha=0.5, ax=axs[i])
+                sns.lineplot(data = data, x='x', y='q025', color='grey', linestyle='--', alpha=0.5, ax=axs[i])
                 
-            plt.subplots_adjust(right=0.85) #make space for the legend
-
-            axs[0].set_title("f(x) = 2x\N{SUBSCRIPT ONE}")
-            axs[1].set_title("f(x) = x\N{SUBSCRIPT TWO}\u00b2")
+            #plt.subplots_adjust(right=0.85) #make space for the legend
+            
+            axs[0].set_title("f(x) = x\N{SUBSCRIPT ONE}\u00b2")
+            axs[1].set_title("f(x) = 2x\N{SUBSCRIPT TWO}")
             axs[2].set_title("f(x) = sen(x\N{SUBSCRIPT THREE})")
             
             theoretical_patch = mpatches.Patch(color='red', label='Theoretical f(x)')
             learned_patch = mpatches.Patch(color='green', label='Mean estimation of f(x)')
-            quantiles = mpatches.Patch(color='grey', label='Confidence Intervals at 95%')
+            quantiles = mpatches.Patch(color='grey', label='2.5% and 97.5% simulation quantiles')
             
             fig.legend(handles=[theoretical_patch, learned_patch, quantiles], loc='lower center', bbox_to_anchor=(0.5, -0.05),
             fancybox=True, shadow=True, ncol=5)
             plt.tight_layout()
             #plt.show(block=True)
-            plt.savefig("./" + type + ".png", dpi = 100, bbox_inches = "tight")
+            plt.savefig("./" + type + ".png", dpi = 300, bbox_inches = "tight")
 
         except Exception as e:
             print(e) 
