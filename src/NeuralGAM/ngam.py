ce271027918feab974d473b88be9301623398831
Inés Ortega-Fernández <iortega@gradiant.org>
2023-01-13 11:34:50 +0100

    Update dataset folder structure and scripts

--- src/NeuralGAM/ngam.py
+++ src/NeuralGAM/ngam.py
@@ -93,7 +93,7 @@ class NeuralGAM(tf.keras.Model):
         #Initialization
         converged = False
         f = X_train*0
-        g = f
+        g = X_train*0
         index = f.columns.values
         it = 1
         self.training_err = list()
@@ -136,6 +136,10 @@ class NeuralGAM(tf.keras.Model):
                     eta = eta - g[index[k]]
                     residuals = Z - eta
 
+                    if self._family == "binomial":
+                        self.feature_networks[k].compile(loss= "mean_squared_error", 
+                                                        optimizer="adam",
+                                                        loss_weights= pd.Series(W))
                     self.feature_networks[k].fit(X_train[X_train.columns[k]], 
                                                  residuals, 
                                                  epochs=1, 
@@ -146,7 +150,7 @@ class NeuralGAM(tf.keras.Model):
                     eta = eta + f[index[k]]  
 
                 # update current estimations
-                g = f
+                g = f.copy(deep=True)
                 eta = self.eta0 + g.sum(axis=1)
       
                 #compute the differences in the predictor at each iteration
@@ -245,6 +249,8 @@ class NeuralGAM(tf.keras.Model):
     def apply_link(self, muhat):
         """ Applies the link function """ 
         if self._family == "binomial":
+            muhat = np.where(muhat > 10, 10, muhat)
+            muhat = np.where(muhat < -10, -10, muhat)
             return np.exp(muhat) / (1 + np.exp(muhat))
         elif self._family == "gaussian":   # identity / gaussian
             return muhat
@@ -256,6 +262,8 @@ class NeuralGAM(tf.keras.Model):
         """ Computes the inverse of the link function """ 
         if self._family == "binomial":
             d = 1 - muhat 
+            d = np.where(muhat <= 0.001, 0.001, muhat)
+            d = np.where(muhat >= 0.999, 0.999, muhat)
             return np.log(muhat/d) 
         elif self._family == "gaussian":   # identity / gaussian
             return muhat
