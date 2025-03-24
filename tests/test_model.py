89de7cf2b88ba881941215d2ebd28fe811b9f3f9
Ines Ortega <iortega@gradiant.org>
2025-03-24 13:36:03 +0100

    update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -17,39 +17,39 @@ class TestNeuralGAM(unittest.TestCase):
 
     def test_fit_gaussian(self):
         model = NeuralGAM(family='gaussian')
-        y, g, eta = model.fit(self.X_train, self.y_train)
-        self.assertEqual(len(y), len(self.y_train))
-        self.assertEqual(len(g), len(self.X_train))
-        self.assertEqual(len(eta), len(self.y_train))
+        model.fit(self.X_train, self.y_train)
+        self.assertEqual(len(model.y), len(self.y_train))
+        self.assertEqual(len(model.feature_contributions), len(self.X_train))
+        self.assertEqual(len(model.eta), len(self.y_train))
 
     def test_fit_binomial(self):
         y_train_binomial = (self.y_train > np.median(self.y_train)).astype(int)
         model = NeuralGAM(family='binomial')
-        y, g, eta = model.fit(self.X_train, y_train_binomial)
-        self.assertEqual(len(y), len(y_train_binomial))
-        self.assertEqual(len(g), len(self.X_train))
-        self.assertEqual(len(eta), len(y_train_binomial))
+        model.fit(self.X_train, y_train_binomial)
+        self.assertEqual(len(model.y), len(y_train_binomial))
+        self.assertEqual(len(model.feature_contributions), len(self.X_train))
+        self.assertEqual(len(model.eta), len(y_train_binomial))
 
     def test_fit_parallel(self):
         model = NeuralGAM(family='gaussian', verbose=0)
-        y, g, eta = model.fit(self.X_train, self.y_train, parallel=True)
-        self.assertEqual(len(y), len(self.y_train))
-        self.assertEqual(len(g), len(self.X_train))
-        self.assertEqual(len(eta), len(self.y_train))
+        model.fit(self.X_train, self.y_train, parallel=True)
+        self.assertEqual(len(model.y), len(self.y_train))
+        self.assertEqual(len(model.feature_contributions), len(self.X_train))
+        self.assertEqual(len(model.eta), len(self.y_train))
 
     def test_fit_sequential(self):
         model = NeuralGAM(family='gaussian', verbose=0)
-        y, g, eta = model.fit(self.X_train, self.y_train, parallel=False)
-        self.assertEqual(len(y), len(self.y_train))
-        self.assertEqual(len(g), len(self.X_train))
-        self.assertEqual(len(eta), len(self.y_train))
+        model.fit(self.X_train, self.y_train, parallel=False)
+        self.assertEqual(len(model.y), len(self.y_train))
+        self.assertEqual(len(model.feature_contributions), len(self.X_train))
+        self.assertEqual(len(model.eta), len(self.y_train))
 
     def test_fit_with_linear_terms(self):
         model = NeuralGAM(family='gaussian', linear_terms=['feature1'])
-        y, g, eta = model.fit(self.X_train, self.y_train)
-        self.assertEqual(len(y), len(self.y_train))
-        self.assertEqual(len(g), len(self.X_train))
-        self.assertEqual(len(eta), len(self.y_train))
+        model.fit(self.X_train, self.y_train)
+        self.assertEqual(len(model.y), len(self.y_train))
+        self.assertEqual(len(model.feature_contributions), len(self.X_train))
+        self.assertEqual(len(model.eta), len(self.y_train))
 
 if __name__ == '__main__':
     unittest.main()
